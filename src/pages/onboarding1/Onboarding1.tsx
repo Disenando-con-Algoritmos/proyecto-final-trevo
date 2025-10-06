5bb09b98e1db8596d710dc303f0d4df986ed3606
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-06 14:53:37 -0500

    feat: Add styles on the onboarding1 container

--- src/pages/onboarding1/Onboarding1.tsx
+++ src/pages/onboarding1/Onboarding1.tsx
@@ -5,9 +5,9 @@ import ContinueButton from "../../components/ContinueButton";
 
 export default function Onboarding1 () {
     return(
-        <div id="full-page">
-            <div id="boarding-container">
-                <div id="boarding2-continer">
+        <div id="full-page" className="h-screen flex items-center justify-center">
+            <div id="boarding-container" className="w-120 h-160 bg-[#9872F0] rounded-3xl shadow-lg relative">
+                <div id="boarding2-continer" className="w-120 h-80 bg-black rounded-3xl absolute bottom-0">
                     <ContinueButton />
                     <p>
                         <Link className="underline" to="entrypoint">
