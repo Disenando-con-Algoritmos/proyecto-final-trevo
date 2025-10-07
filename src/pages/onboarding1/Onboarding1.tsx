c9d1220f53495158d4c92b3e8ad871c77a428210
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-06 22:01:49 -0500

    feat: Add onboarding1 information text to the secondary container

--- src/pages/onboarding1/Onboarding1.tsx
+++ src/pages/onboarding1/Onboarding1.tsx
@@ -6,12 +6,14 @@ import ContinueButton from "../../components/ContinueButton";
 export default function Onboarding1 () {
     return(
         <div id="full-page" className="h-screen flex items-center justify-center">
-            <div id="boarding-container" className="w-120 h-160 bg-[#9872F0] rounded-3xl shadow-lg relative">
-                <div id="boarding2-continer" className="w-120 h-80 bg-black rounded-3xl absolute bottom-0">
+            <div id="boarding-container" className="w-120 h-150 bg-[#9872F0] rounded-3xl shadow-lg relative">
+                <div id="boarding2-continer" className="w-120 h-70 bg-black rounded-3xl absolute bottom-0 flex flex-col items-center justify-start gap-5 px-8 pt-20">
+                    <h1 className="text-3xl">Be a fitter version</h1>
+                    <h5 className="text-sm">Post your progress and stay motivated everyday</h5>
                     <ContinueButton />
                     <p>
                         <Link className="underline" to="entrypoint">
-                            skip
+                            Skip
                         </Link>
                     </p>
                 </div>
