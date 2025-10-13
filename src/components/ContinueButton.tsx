4770aee2dd4e30c9f118cba28c75ccfd91e9dbe8
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-12 23:16:14 -0500

    feat: Add the continue button to the onboarding2 components folder

---
+++ src/components/ContinueButton.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+export default function ContinueButton() {
+    return (
+        <div id="continue-button">
+            <Link to="/onboarding2">
+                <button className="bg-[#A480FF] text-white font-semibold text-base rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                    Continue
+                </button>
+            </Link>
+        </div>
+    );
+}
