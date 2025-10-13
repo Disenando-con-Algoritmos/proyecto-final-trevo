fceb6c2df6a87ac80ae050ebb5bd8736a5a7cdfb
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-13 12:20:27 -0500

    feat: Add all elements to entrypoint: image, heading, buttons

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,10 +4,11 @@ export default function LoginButton() {
     return (
         <div id="login-button">
             <Link to="/login">
-                <button className="bg-[#A480FF] text-white font-semibold text-base rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                <button className="border-2 border-[#9872F0] text-[#ffffff] font-semibold text-base rounded-full px-25 py-1.5 bg-transparent transition duration-300 hover:bg-[#9872F0] hover:text-white active:scale-95">
                     Log in
                 </button>
             </Link>
         </div>
     );
 }
+
--- src/pages/entrypoint/EntryPoint.tsx
+++ src/pages/entrypoint/EntryPoint.tsx
@@ -1,8 +1,23 @@
+import CreateButton from "../../components/CreateButton";
+import LoginButton from "../../components/LoginButton";
 
-export default function EntryPoint () {
-    return(
-        <div id="entrypoint-page">
-            <h1 className="text-4xl">Entry Point</h1>
+import background from "./background.png";
+import logo from "./logo.png";
+
+export default function Entrypoint () {
+    return (
+        <div id="full-page" className="h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
+            <div id="entry-container" className="w-120 h-133 bg-[#121212] rounded-3xl shadow-lg relative">
+                <h1 className="text-[60px] text-center p-8 font-bold">Welcome to</h1>
+                <h2 className="text-[60px] font-bold text-center text-[#CAD83B] -mt-[60px]">Trevo</h2>
+                <img src={logo} alt="logo ilustration" className="h-15 w-15 mx-auto mt-[30px]" />
+                <h1 className="text-[30px] w-80 text-center leading-[34px] mx-auto mt-[30px]">Where fitness meets friends</h1>
+
+                <div className="mt-6 flex flex-col items-center gap-[10px]">
+                    <CreateButton />
+                    <LoginButton />
+                </div>
+            </div>
         </div>
     );
 }
