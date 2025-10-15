dec0b49b2cd1501199cccfde2c9a0394408ed0ae
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-15 02:52:05 -0500

    feat: Implement JSON for posts
    Connected post json with services and types.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,24 @@
 import { Bell } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import NavBar from "../../components/NavBar";
 import PostCard from "../../components/PostCard";
+import type { Posttype } from "../../types/postTypes";
+import { getPosts } from "../../services/postServices";
 
 import ContainerHashtag from "./ContainerHashtag";
 
 export default function Home () {
+    const [posts, setPosts] = useState<Posttype[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getPosts();
+            setPosts(data);
+        };
+        fetchData();
+    }, []);
+
     return(
         <div id="home-page" className="min-h-screen m-0 p-0 flex flex-row font-[neulis] bg-[#1E1E1E]">
             <div id="navbar" className="fixed top-0 left-0 h-full w-[330px]">
@@ -26,7 +39,9 @@ export default function Home () {
             </div>
 
             <div className="ml-[320px] mt-[200px] top-0 bg-[#1E1E1E] w-[600px] h-min">
-                <PostCard/>
+                {posts.map((post: Posttype) => {
+                    return <PostCard key={post.id} post={post} />;
+                })}
             </div>
         </div>
     );
---
+++ src/types/postTypes.ts
@@ -0,0 +1,12 @@
+type Posttype = {
+    id: string;
+    profilepic: string;
+    username: string;
+    description: string;
+    image: string;
+    likes: string;
+    comments: number;
+    hashtag: string;
+};
+
+export type { Posttype };
