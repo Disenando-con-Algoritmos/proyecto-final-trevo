890c8b2bf29f56c186d2addd62420a3011b5e75d
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 17:59:12 -0500

    fix: Modify typography on buttons

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -4,7 +4,7 @@ export default function CreateButton() {
     return (
         <div id="create-button">
             <Link to="/register">
-                <button className="bg-[#A480FF] text-white font-semibold text-base rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                <button className="bg-[#A480FF] text-white font-[poppins] text-base rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
                     Create an account
                 </button>
             </Link>
