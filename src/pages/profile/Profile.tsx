1d9fa8974df06ce1f993bdab7fa8bfe5584cb5e8
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-15 19:29:12 -0500

    style: Add styles with implemented navbar

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,19 @@
+import NavBar from "../../components/NavBar";
+
 import background from "./background.png";
 
-export default function Profile () {
-    return(
-        <div id="full-page" className="h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
+export default function Profile() {
+    return (
+        <div id="full-page" className="h-screen w-screen relative" style={{ backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundPosition: "center" }}>
+            <div id="navbar" className="fixed top-0 left-0 h-full w-[330px]">
+                <NavBar />
+            </div>
+
+            <div id="content" className="ml-[330px] flex flex-col p-4 min-h-screen text-white font-[neulis]">
+                <div id="info" className="w-[580px] h-[200px] bg-[#1E1E1E] p-4">
+                    <h1>hola</h1>
+                </div>
+            </div>
         </div>
     );
 }
