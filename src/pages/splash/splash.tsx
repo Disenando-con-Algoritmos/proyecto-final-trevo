337e0efb482cfc7352fb1870103cf9417c54e10a
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-15 21:25:55 -0500

    feat: Add splash screen

--- src/pages/splash/splash.tsx
+++ src/pages/splash/splash.tsx
@@ -1,7 +1,19 @@
-export default function Splash () {
-    return(
-        <div id="splash-page">
-            <h1 className="text-4xl">Splash</h1>
-        </div>
+import { Link } from "react-router";
+import backgroundSplash from "/assets/backgroundsplash.png";
+import logo from "/assets/logo.png";
+
+export default function Splash() {
+    return (
+        <Link to="/entrypoint">
+            <div className="w-screen h-screen relative cursor-pointer">
+                <div className="absolute inset-0 bg-black">
+                    <img src={backgroundSplash} alt="Fondo" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <img src={logo} alt="Logo" className="w-20 h-20 object-contain" />
+                </div>
+            </div>
+        </Link>
     );
 }
