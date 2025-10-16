7b296b281024a4b9aa548d0009213fd85112a3bc
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-15 22:34:15 -0500

    style: Add media queries to splash screen

--- src/pages/splash/splash.tsx
+++ src/pages/splash/splash.tsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router";
-import backgroundSplash from "/assets/backgroundsplash.png";
-import logo from "/assets/logo.png";
+import { useMediaQuery } from "@mui/material";
 
 export default function Splash() {
+    const matches = useMediaQuery("(min-width:600px)");
+
     return (
         <Link to="/entrypoint">
             <div className="w-screen h-screen relative cursor-pointer">
                 <div className="absolute inset-0 bg-black">
-                    <img src={backgroundSplash} alt="Fondo" className="w-full h-full object-cover" />
+                    <img src="/trevo/assets/backgroundsplash.png" alt="Fondo" className="w-full h-full object-cover" />
                 </div>
 
                 <div className="absolute inset-0 flex items-center justify-center">
-                    <img src={logo} alt="Logo" className="w-20 h-20 object-contain" />
+                    <img src= "/trevo/assets/logo.png" alt="Logo" className={`${matches ? "w-22 h-22" : "w-35 h-35"} object-contain`} />
                 </div>
             </div>
         </Link>
