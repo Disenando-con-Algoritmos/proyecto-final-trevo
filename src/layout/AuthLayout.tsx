28d09ff6fb3f46343df7debd910bdec04dbbec4f
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-17 04:28:34 -0500

    fix: Apply corrections for merge

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router";
 export default function AuthLayout() {
     const isAuthenticated = localStorage.getItem("user");
 
-    return <>{isAuthenticated ? <Outlet /> : <Navigate to="/entrypoint" />}</>;
+    return <>{isAuthenticated ? <Outlet /> : <Navigate to="/" />}</>;
 }
