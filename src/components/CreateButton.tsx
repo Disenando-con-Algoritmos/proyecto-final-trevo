f54a42f668313813d0b385397aa7597cae480020
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 22:27:51 -0500

    style: Add pointer style to buttons

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -4,7 +4,7 @@ export default function CreateButton() {
     return (
         <div id="create-button">
             <Link to="/signup">
-                <button className="bg-[#A480FF] text-white font-[poppins] text-base rounded-full px-15 py-2.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                <button className="bg-[#A480FF] text-white font-[poppins] text-base rounded-full px-15 py-2.5 transition duration-300 hover:bg-[#956CFF] active:scale-95 cursor-pointer">
                     Create account
                 </button>
             </Link>
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,7 +4,7 @@ export default function LoginButton() {
     return (
         <div id="login-button">
             <Link to="/login">
-                <button className="border-2 border-[#9872F0] text-white font-poppins rounded-full px-25 py-2.5 bg-transparent transition duration-300 hover:bg-[#9872F0] hover:text-white active:scale-95">Log in</button>
+                <button className="border-2 border-[#9872F0] text-white font-poppins rounded-full px-25 py-2.5 bg-transparent transition duration-300 hover:bg-[#9872F0] hover:text-white active:scale-95 cursor-pointer">Log in</button>
             </Link>
         </div>
     );
