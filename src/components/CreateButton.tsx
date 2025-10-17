b12ad1e81432e6cb9c8bb4b120bcb79bb9181144
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 19:23:14 -0500

    fix: Modify button sizes an typography

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -4,8 +4,8 @@ export default function CreateButton() {
     return (
         <div id="create-button">
             <Link to="/register">
-                <button className="bg-[#A480FF] text-white font-[poppins] text-base rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
-                    Create an account
+                <button className="bg-[#A480FF] text-white font-[poppins] text-base rounded-full px-15 py-2.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                    Create account
                 </button>
             </Link>
         </div>
