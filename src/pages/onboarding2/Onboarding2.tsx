606b08485a96616bb4d909834314fa6f620c6d5f
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 21:51:37 -0500

    fix: Modify button names to avoid conflicts between branches

--- src/pages/onboarding2/Onboarding2.tsx
+++ src/pages/onboarding2/Onboarding2.tsx
@@ -1,5 +1,5 @@
-import ContinueButton from "../../components/ContinueButton";
-import DotsIndicator from "../../components/DotsIndicator";
+import ContinueButton2 from "../../components/ContinueButton2";
+import DotsIndicator2 from "../../components/DotsIndicator2";
 
 export default function Onboarding1 () {
     return (
@@ -9,12 +9,12 @@ export default function Onboarding1 () {
                 <img src="/trevo/assets/image4.png" alt="Onboarding title" className="absolute left-1/2 transform -translate-x-1/2 w-70 h-auto object-contain top-[5%]" />
                 <div id="boarding2-continer" className="w-120 h-70 bg-black rounded-3xl absolute bottom-0 flex flex-col items-center justify-start gap-5 px-8 pt-20">
                     <div id="dots-indicator" className="-mt-16">
-                        <DotsIndicator active={1} />
+                        <DotsIndicator2 active={1} />
                     </div>
                     <h1 className="text-3xl font-[neulis]">Connect with pals</h1>
                     <h5 className="text-sm w-60 text-center font-[poppins]">Meet people who share your vibe and grow together.</h5>
                     <div className="mt-3">
-                        <ContinueButton />
+                        <ContinueButton2 />
                     </div>
 
                 </div>
