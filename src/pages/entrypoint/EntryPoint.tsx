10b22c1d2f05a7e5f544b89c3e4be795af6a12cc
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 22:24:25 -0500

    fix: Modify the name of the button

--- src/pages/entrypoint/EntryPoint.tsx
+++ src/pages/entrypoint/EntryPoint.tsx
@@ -7,7 +7,7 @@ export default function Entrypoint () {
             <div id="entry-container" className="w-120 h-133 bg-[#121212] rounded-3xl shadow-lg relative">
                 <h1 className="text-[60px] text-center p-8 font-[neulis]">Welcome to</h1>
                 <h2 className="text-[60px] text-center text-[#CAD83B] -mt-[60px] font-[neulis]">Trevo</h2>
-                <img src="/trevo/assets/logo.png" alt="logo ilustration" className="h-15 w-15 mx-auto mt-[20px]" />
+                <img src="/trevo/assets/logoentrypoint.png" alt="logo ilustration" className="h-15 w-15 mx-auto mt-[20px]" />
                 <h1 className="text-[25px] w-80 text-center leading-[34px] mx-auto mt-[30px] font-[poppins]">Where fitness meets friends</h1>
 
                 <div className="mt-6 flex flex-col items-center gap-[10px]">
