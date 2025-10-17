b8963b2aabd8811df162c2d482a43ccd8d710770
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-17 04:29:42 -0500

    fix: Adjust code before merge

--- src/layout/AuthLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router";
-
-export default function AuthLayout() {
-    const isAuthenticated = localStorage.getItem("user");
-
-    return <>{isAuthenticated ? <Outlet /> : <Navigate to="/" />}</>;
-}
