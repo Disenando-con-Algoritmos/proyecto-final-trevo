d18d35ac7cd9a316ea6eff5ee8720dc964c57efe
Jorgevasco246 <1107844162@u.icesi.edu.co>
2025-10-17 00:08:58 -0500

    feat: Add the links to the login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from "react";
-import { useNavigate } from "react-router";
-import { TextField, InputAdornment, IconButton,} from "@mui/material";
+import { Link, useNavigate } from "react-router";
+import { TextField, InputAdornment, IconButton, } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 import { BtnLogin } from "../../components/BtnLogin";
 
-import fondoLogin from "./fongoL.png";
+import fondoLogin from "./FondoL.png";
 
 export default function Login() {
     const formRef = useRef(null);
@@ -25,7 +25,7 @@ export default function Login() {
             const formData = new FormData(formResponse);
             localStorage.setItem("username", formData.get("username") as string);//No funciono
 
-            nav("/home");
+            nav("/auth/home");
         }
     };
 
@@ -34,7 +34,7 @@ export default function Login() {
         <div className="flex min-h-screen">
             <div
                 className="w-1/2 bg-no-repeat bg-cover bg-center"
-                style={{   
+                style={{
                     backgroundImage: `url(${fondoLogin})`,
                     backgroundPosition: "center 92%",
                 }}
@@ -42,24 +42,46 @@ export default function Login() {
 
             <div className="w-1/2 flex flex-col justify-center items-center bg-[#111] text-white p-24">
                 <h1 className="text-5xl font-medium mb-4 font-[Neulis]">Welcome Back</h1>
-                <h4 className="text-gray-400 mb-1 font-[poppins]">
-            Ready to continue your fitness journey?
+                <h4 className="text-white mb-1 text-sm font-[poppins]">
+                    Ready to continue your fitness journey?
                 </h4>
-                <h4 className="text-gray-400 mb-6 font-[poppins]">Your path is right here</h4>
-
+                <h4 className="text-white text-sm font-[poppins]">Your path is right here</h4>
+                <div className="mb-[15px]">
+                </div>
+                <div className="mb-[15px]">
+                </div>
                 <form
                     ref={formRef}
                     onSubmit={handleSubmit}
-                    className="flex flex-col justify-center items-center gap-3 w-[400px] max-w-md font-[poppins]">
-                    
+                    className="flex flex-col justify-center items-center gap-3 w-[400px] max-w-md font-[poppins] ">
+
                     <TextField
                         fullWidth
                         label="Enter email or username"
                         variant="outlined"
                         className="font-[poppins]"
-                        color="secondary" focused 
                         name="username"
-                        sx={{input: { color: "white" }
+                        sx={{
+                            input: { color: "white" },
+                            "& .MuiOutlinedInput-root": {
+                                borderRadius: "15px",
+                                backgroundColor: "transparent",
+                                "&:hover": {
+                                    backgroundColor: "transparent",
+                                },
+                                "& fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                                "&:hover fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                                "&.Mui-focused fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                            },
+                            "& .MuiInputLabel-root": {
+                                color: "gray",
+                            },
                         }}
                     />
 
@@ -68,47 +90,76 @@ export default function Login() {
                         type={showPassword ? "text" : "password"}
                         label="Password"
                         variant="outlined"
-                        color="secondary" focused
                         className="font-[poppins]"
                         name="password"
                         sx={{
                             input: { color: "white" },
+                            "& .MuiOutlinedInput-root": {
+                                borderRadius: "15px",
+                                backgroundColor: "transparent",
+                                "&:hover": {
+                                    backgroundColor: "transparent",
+                                },
+                                "& fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                                "&:hover fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                                "&.Mui-focused fieldset": {
+                                    borderColor: "#9b7ff5",
+                                },
+                            },
+                            "& .MuiInputLabel-root": {
+                                color: "gray",
+                            },
                         }}
+                        //InputProps sirve para personalizar el campo de entrada interno
                         InputProps={{
+                            //endAdornment sirve para agregar elementos visuales al final de un campo de entrada
                             endAdornment: (
                                 <InputAdornment position="end">
-                                    <IconButton
-                                    //handleClickShowPassword
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        //handleMouseDownPassword
-                                        edge="end">
-                                        {showPassword ? (
-                                            <Visibility sx={{ color: "purple" }} />
-                                        ) : (
-                                            <VisibilityOff sx={{ color: "white" }} />
-                                        )}
+                                    <IconButton onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}>
+                                        {showPassword ? <Visibility sx={{ color: "purple" }} /> : <VisibilityOff sx={{ color: "white" }} />}
                                     </IconButton>
                                 </InputAdornment>
                             ),
-                        }}/>
+                        }}
+                    />
 
-                    <div className="flex items-center mb-2 self-start">
-                        <input
-                            id="remember-me"
-                            type="checkbox"
-                            className="w-4 h-4 text-[#9b7ff5] bg-gray-100 border-gray-300 rounded focus:ring-[#9b7ff5] focus:ring-2"
-                        />
-                        <label
-                            htmlFor="remember-me"
-                            className="ml-2 text-sm font-medium text-gray-300">
-                    Remember me
-                        </label>
+                    <div className="mb-[2px]">
+                    </div>
+                    <div className="flex justify-between items-center w-full mb-6">
+                        <div className="flex items-center">
+                            <input
+                                id="remember-me"
+                                type="checkbox"
+                                className="w-4 h-4 text-[#00f328] bg-green-500 border-green-500 rounded"
+                            />
+                            <label
+                                htmlFor="remember-me"
+                                className="ml-2 text-sm font-medium text-green-100 font-[poppins]"
+                            >
+                                Remember me
+                            </label>
+                        </div>
+
+                        <a href="" className="text-sm text-white  font-[poppins]">
+                            Forgot password?
+                        </a>
                     </div>
 
                     <BtnLogin />
-                    
+
                 </form>
+                <div className="mb-[15px]">
+                </div>
+                <p className="text-white pt-2 text-[15px] font-[poppins]">
+                    Don’t have an account?{" "}
+                    <Link to="/signup" className="pt-2 text-purple-500 underline font-[poppins]">
+                        Sign Up
+                    </Link>
+                </p>
             </div>
         </div>
     );
