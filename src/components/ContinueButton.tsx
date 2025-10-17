b90d3c3034691f785155d8b40043209c501fcaa7
mceronvillegas <1110366789@u.icesi.edu.co>
2025-10-16 19:37:36 -0500

    fix: Edit font style on the continue button

--- src/components/ContinueButton.tsx
+++ src/components/ContinueButton.tsx
@@ -4,7 +4,7 @@ export default function ContinueButton() {
     return (
         <div id="continue-button">
             <Link to="/onboarding2">
-                <button className="bg-[#A480FF] text-white font-[poppins] font-bold rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
+                <button className="bg-[#A480FF] text-white font-[poppins] rounded-full px-15 py-1.5 transition duration-300 hover:bg-[#956CFF] active:scale-95">
                     Continue
                 </button>
             </Link>
