543ebfb28799ec36672140bc48e0bdef2f48b1ab
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-11-06 02:14:29 -0500

    style: Update arrow icon and spacing

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,14 +1,9 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router";
-import {
-    TextField,
-    InputAdornment,
-    IconButton,
-    Checkbox,
-    FormControlLabel,
-} from "@mui/material";
+import { TextField, InputAdornment, IconButton, Checkbox, FormControlLabel } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { ChevronLeft } from "lucide-react";
 
 import { BtnSignUp } from "../../components/BtnSignUp";
 import { getUsers } from "../../services/userServices";
@@ -89,54 +84,31 @@ export default function Signup() {
 
     return (
         <div className={`flex h-screen ${matches ? "flex-row overflow-x-hidden box-border" : "flex-col bg-[#111]"}`}>
+            <Link to="/entrypoint" className="absolute top-6 left-6 p-2 mt-15 bg-opacity-20 backdrop-blur-sm z-10 hover:bg-opacity-30 transition">
+                <ChevronLeft size={40} color="#CAD83B" strokeWidth={2.2} className="hover:scale-105 transition-transform" />
+            </Link>
             <div
                 className={`relative bg-no-repeat bg-cover bg-center ${matches ? "w-1/2 h-full overflow-hidden" : "w-full h-[220px] rounded-b-[40px] z-0"}`}
                 style={{
                     backgroundImage: "url(/trevo/assets/backgroundlogin.png)",
                 }}
             >
-                <div className={`${matches ? "absolute left-[100px] top-[200px]" : "flex flex-col justify-center items-center h-full"}`}>
-                    {matches ? (
-                        <img
-                            src="/trevo/assets/logintitle.png"
-                            alt="Trevo Title"
-                            className="object-contain"
-                            style={{ width: "100%", maxWidth: 480, height: "auto" }}
-                        />
-                    ) : (
-                        <h1 className="text-[50px] font-[Neulis] text-[#CAD83B]">Trevo</h1>
-                    )}
+                <div className={`font-[Neulis] ${matches ? "absolute left-[100px] top-[200px]" : "flex flex-col justify-center items-center h-full"}`}>
+                    {matches ? <img src="/trevo/assets/logintitle.png" alt="Trevo Title" className="object-contain" style={{ width: "100%", maxWidth: 480, height: "auto" }} /> : <h1 className="text-[50px] font-bold text-[#CAD83B]">Trevo</h1>}
                 </div>
 
                 {matches && (
                     <div className="absolute bottom-0 left-[110px] overflow-hidden" style={{ maxWidth: "60%" }}>
-                        <img
-                            src="/trevo/assets/loginilustration.png"
-                            alt="Signup ilustration"
-                            className="object-contain"
-                            style={{ width: "100%", maxWidth: 600, height: "auto", display: "block" }}
-                        />
+                        <img src="/trevo/assets/loginilustration.png" alt="Signup ilustration" className="object-contain" style={{ width: "100%", maxWidth: 600, height: "auto", display: "block" }} />
                     </div>
                 )}
             </div>
-            <div
-                className={`flex flex-col justify-center items-center text-white bg-[#111] ${matches ? "w-1/2 px-16 py-12 h-full overflow-y-auto box-border" : "w-full px-8 py-10 rounded-t-[40px] -mt-12 z-10 flex-grow overflow-y-auto"}`}
-            >
-                <h1 className={`${matches ? "text-5xl" : "text-3xl"} font-medium mt-8 mb-4 font-[Neulis] text-center w-full max-w-md`}>
-                    Create account
-                </h1>
-                <p className="text-white mb-1 text-sm font-[poppins] text-center w-full max-w-md">
-                    We’re here to help you reach the peaks
-                </p>
-                <p className="text-white text-sm font-[poppins] text-center mb-8 w-full max-w-md">
-                    of fitness. Are you ready?
-                </p>
+            <div className={`flex flex-col justify-center items-center text-white bg-[#111] ${matches ? "w-1/2 px-16 py-12 h-full overflow-y-auto box-border" : "w-full px-8 py-10 rounded-t-[40px] -mt-12 z-10 flex-grow overflow-y-auto"}`}>
+                <h1 className={`${matches ? "text-5xl" : "text-3xl"} font-medium mt- mb-4 font-[Neulis] text-center w-full max-w-md`}>Create account</h1>
+                <p className="text-white mb-1 text-sm font-[poppins] text-center w-full max-w-md">We’re here to help you reach the peaks</p>
+                <p className="text-white text-sm font-[poppins] text-center mb-8 w-full max-w-md">of fitness. Are you ready?</p>
 
-                <form
-                    ref={formRef}
-                    onSubmit={handleSubmit}
-                    className="flex flex-col justify-center items-center gap-3 w-[90%] md:w-[400px] max-w-md font-[poppins]"
-                >
+                <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-3 w-[90%] md:w-[400px] max-w-md font-[poppins]">
                     <TextField fullWidth label="Enter username" variant="outlined" name="username" sx={textFieldStyle} />
                     <TextField fullWidth label="Enter email" variant="outlined" name="email" type="email" sx={textFieldStyle} />
                     <TextField
@@ -157,7 +129,7 @@ export default function Signup() {
                         }}
                     />
 
-                    <div className="flex justify-start w-full mb-8 mt-1">
+                    <div className="flex justify-start w-full mb-12 mt-1">
                         <FormControlLabel
                             control={<Checkbox id="remember-me" name="remember-me" sx={{ color: "#CAD83B", "&.Mui-checked": { color: "#CAD83B" }, "& .MuiSvgIcon-root": { borderRadius: "50%" } }} />}
                             label="Remember me"
