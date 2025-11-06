f007d5f9caef2b1f21109477cc7662172fd2367e
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-11-06 01:58:48 -0500

    style: Update login colors and spaces

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState, useEffect } from "react";
-// Asegúrate de importar desde 'react-router-dom'
 import { Link, useNavigate } from "react-router";
 import { TextField, InputAdornment, IconButton, Checkbox, FormControlLabel } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { ChevronLeft } from "lucide-react";
 
 import { BtnLogin } from "../../components/BtnLogin";
 import { getUsers } from "../../services/userServices";
@@ -52,64 +52,47 @@ export default function Login() {
 
     return (
         <div className={`flex min-h-screen overflow-hidden ${matches ? "flex-row" : "flex-col bg-[#111]"}`}>
-            
             {matches && (
                 <div
-                    className="w-1/2 bg-no-repeat bg-cover bg-center relative" 
+                    className="w-1/2 bg-no-repeat bg-cover bg-center relative"
                     style={{
                         backgroundImage: "url(/trevo/assets/backgroundlogin.png)",
                     }}
                 >
                     <div className="absolute left-[100px] top-[200px]">
-                        <img 
-                            src="/trevo/assets/logintitle.png" 
-                            alt="login title" 
-                            className="w-[260px] md:w-[320px] lg:w-[480px] object-contain" 
-                        />
+                        <img src="/trevo/assets/logintitle.png" alt="login title" className="w-[260px] md:w-[320px] lg:w-[480px] object-contain" />
                     </div>
                     <div className="absolute bottom-0 left-[110px] w-[65%]">
-                        <img 
-                            src="/trevo/assets/loginilustration.png" 
-                            alt="login ilustration" 
-                            className="w-[600px]" 
-                        />
+                        <img src="/trevo/assets/loginilustration.png" alt="login ilustration" className="w-[600px]" />
                     </div>
                 </div>
             )}
 
             {!matches && (
-                <div className="relative w-full h-[220px] flex flex-col justify-center items-center rounded-b-[40px]"
+                <div
+                    className="font-[Neulis] relative w-full h-[220px] flex flex-col justify-center items-center rounded-b-[40px]"
                     style={{
                         backgroundImage: "url(/trevo/assets/backgroundlogin.png)",
                         backgroundSize: "cover",
                         backgroundPosition: "center",
-                    }}>
-                    <h1 className="text-[50px] font-[Neulis] text-[#CAD83B] mt-07">Trevo</h1>
+                    }}
+                >
+                    <Link to="/entrypoint" className="absolute top-6 left-6 p-2 mt-15 bg-opacity-20 backdrop-blur-sm z-10 hover:bg-opacity-30 transition">
+                        <ChevronLeft size={40} color="#CAD83B" strokeWidth={2.2} className="hover:scale-105 transition-transform" />
+                    </Link>
+
+                    <h1 className="text-[50px] font-bold text-[#CAD83B] mt-07">Trevo</h1>
                 </div>
             )}
-            <div 
+            <div
                 className={`flex flex-col justify-center items-center text-white bg-[#111]
-                    ${matches 
-            ? "w-1/2 p-24" // Estils pc
-            : "w-full px-8 py-10 rounded-t-[40px] -mt-12 z-10" // Estilos Mvil
-        }`
-                }
+                    ${matches ? "w-1/2 p-24" : "w-full px-8 py-10 rounded-t-[40px] -mt-12 z-10"}`}
             >
-                <h1 className={`${matches ? "text-5xl" : "text-3xl"} font-medium mt-8 mb-4 font-[Neulis] text-center`}>
-                    Welcome Back
-                </h1>
-                <p className="text-white mb-1 text-sm font-[poppins] text-center">
-                    Ready to continue your fitness journey?
-                </p>
-                <p className="text-white text-sm font-[poppins] text-center mb-8">
-                    Your path is right here
-                </p>
+                <h1 className={`${matches ? "text-5xl" : "text-3xl"} font-medium mt-8 mb-4 font-[Neulis] text-center`}>Welcome Back</h1>
+                <p className="text-white mb-1 text-sm font-[poppins] text-center">Ready to continue your fitness journey?</p>
+                <p className="text-white text-sm font-[poppins] text-center mb-8">Your path is right here</p>
 
-                <form 
-                    ref={formRef} 
-                    onSubmit={handleSubmit} 
-                    className="flex flex-col justify-center items-center gap-3 w-[90%] md:w-[400px] max-w-md font-[poppins]"
-                >
+                <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-3 w-[90%] md:w-[400px] max-w-md font-[poppins]">
                     <TextField
                         fullWidth
                         label="Enter email or username"
@@ -154,34 +137,31 @@ export default function Login() {
                         }}
                     />
 
-                    <div className="flex justify-between items-center w-full mb-4 mt-1">
+                    <div className="flex justify-between items-center w-full mb-30 mt-1">
                         <FormControlLabel
                             control={
-                                <Checkbox 
+                                <Checkbox
                                     id="remember-me"
                                     sx={{
-                                        color: "#CAD83B", // (verde lima)
+                                        color: "#CAD83B",
                                         "&.Mui-checked": { color: "#CAD83B" },
-                                        "& .MuiSvgIcon-root": { borderRadius: "50%" } 
+                                        "& .MuiSvgIcon-root": { borderRadius: "50%" },
                                     }}
                                 />
                             }
                             label="Remember me"
                             sx={{
-                                "& .MuiFormControlLabel-label": { 
-                                    color: "white", 
-                                    fontSize: "0.875rem", 
-                                    fontFamily: "poppins" 
-                                }
+                                "& .MuiFormControlLabel-label": {
+                                    color: "white",
+                                    fontSize: "0.875rem",
+                                    fontFamily: "poppins",
+                                },
                             }}
                         />
-                        <Link to="/forgot-password" className="text-sm text-purple-500 underline font-[poppins]">
-                            Forgot password?
-                        </Link>
                     </div>
 
                     <BtnLogin />
-                    
+
                     <p className="text-white pt-2 text-[12px] font-[poppins] text-center">
                         Don’t have an account?{" "}
                         <Link to="/signup" className="text-[#9872F0] underline font-[poppins]">
